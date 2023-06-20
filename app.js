8b4f5df2eff3d9076ae5b74c8f123d9bae2d931f
christiansanguinetti <christiansanguinetti882@gmail.com>
2023-06-20 20:05:18 -0300

    error

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+
+
+app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 
@@ -7,9 +10,12 @@ app.get('hola-mundo', function (req, res) {
     res.send('Hola mundo en su respectiva ruta')
   })
 
-app.get('/generic', (req, res)=> {
-res.sendFile(__dirname + '/public/generic.html');
+app.get('/', (req, res)=> {
+res.render('home');
 });
+app.get('/generic', (req, res)=> {
+  res.sendFile(__dirname + '/public/generic.html');
+  });
 app.get('/elements', (req, res)=> {
     res.sendFile(__dirname + '/public/elements.html');
     });
