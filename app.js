43070689c24a693b6a55524f0724c6dcb366e9ae
christiansanguinetti <christiansanguinetti882@gmail.com>
2023-06-20 19:49:56 -0300

    cambios

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ app.get('hola-mundo', function (req, res) {
     res.send('Hola mundo en su respectiva ruta')
   })
 
-app.get('*', (req, res)=> {
-res.sendFile(__dirname + '/public/404.html');
+app.get('/generic', (req, res)=> {
+res.sendFile(__dirname + '/public/generic.html');
 });
-
+app.get('/elements', (req, res)=> {
+    res.sendFile(__dirname + '/public/elements.html');
+    });
+    
 app.listen(8080)
