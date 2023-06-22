fad97cebc6701d74ddf77ee3d3fa6a42351c36ce
christiansanguinetti <christiansanguinetti882@gmail.com>
2023-06-22 19:10:36 -0300

    cambis

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs');
 
-
+//handl
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 app.use(express.static('public'));
 
-
+//servir contenido estatico
 app.get('hola-mundo', function (req, res) {
-    res.send('Hola mundo en su respectiva ruta')
-  })
+  res.send('Hola mundo en su respectiva ruta')
+})
 
-app.get('/', (req, res)=> {
-res.render('home');
-});
-app.get('/generic', (req, res)=> {
-  res.sendFile(__dirname + '/public/generic.html');
+app.get('/', (req, res) => {
+  res.render('home',{
+    Nombre: 'Christian Sanguinetti',
+    titulo: 'Curso Node'
   });
-app.get('/elements', (req, res)=> {
-    res.sendFile(__dirname + '/public/elements.html');
-    });
-    
+});
+app.get('/public/back/templatrte/generic.html', (req, res) => {
+  res.render('generic',{
+    Nombre: 'Christian Sanguinetti',
+    titulo: 'Curso Node'
+  });
+});
+app.get('/public/back/templatrte/elements.html', (req, res) => {
+  res.render('elements',{
+    Nombre: 'Christian Sanguinetti',
+    titulo: 'Curso Node'
+  });
+});
 app.listen(8080)
